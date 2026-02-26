97a38c3fa26a2ab38025420663e590da16578369
T. L. Bainter <taran.bainter@gmail.com>
2026-02-26 12:50:25 -0600

    Player-Controlled Camera Panning/Zooming
    Implemented player-controlled camera panning and zooming. Press [R3]/[Home] to zoom out within a set limit or use [Arrow Keys]/[Right Joystick] to pan the camera around the player.
    
    The challenge here was ensuring the limitations remained within the viewport bounds that were already determined without looking awkward.
    
    Still needs a bit of work on the zoom-out mechanic, but not entirely sure what I want that to look like with regards to recentering the player when you are near the bounds of an area. Testing needed.

--- Godot/zelda-like-project/Scripts/Classes/Camera Class/Player Camera/Camera_Player.gd
+++ Godot/zelda-like-project/Scripts/Classes/Camera Class/Player Camera/Camera_Player.gd
@@ -12,18 +12,37 @@ extends CamClass
 ##The players input controller.
 @onready var input : InputComponent = root.input
 @export_group("PlayerCam Variables")
+@export_subgroup("DeadZones")
 ##Where you want the camera to stop horizontally.
 @export var horizontal_dead_zone : float = 30.0
 ##Where you want the camera to stop vertically.
 @export var vertical_dead_zone : float = 30.0
+@export_subgroup("Pan and Zoom")
+## The base resting zoom of the camera.[br]
+## You should set this to match the zoom of the camera in-editor.
+@export var init_zoom : Vector2 = zoom
+## The maximum number, in pixels, that the camera can pan in any direction.
+@export var max_pan_distance : Vector2 = Vector2(100.0, 80.0)
+##How quickly the camera smoothly arrives at its destination
+@export var smoothness : float = 12.0
+## target offset value for pan and zoom lerping
+var _target_offset : Vector2 = Vector2.ZERO
+var _target_zoom : Vector2 = Vector2(1.0, 1.0)
 #TODO: Make the ground variable an array, someday—or something to that effect. Could be a dictionary?[br]
 #This will be necessary if I am going to have more than one ground tilemap layer in a single scene!
+@export_subgroup("Detection")
 ##The ground layer of the level.
 var ground : GroundTilemap
+#endregion VARIABLES
+
+#region FUNCTIONS
 
 #Initialize position
 func _ready() -> void:
 	#Get a reference to the GroundTilemap of a level on ready.
+	#TEST: Testing whether or not this has significant impact on the camera's functionality.
+	top_level = true
+	reset_zoom()
 	set_ground()
 	set_cam_limits()
 	#TODO: Use the input signal to call the camera movement.
@@ -34,8 +53,16 @@ func _ready() -> void:
 #This will limit resource drain. Calling this every frame is dicey.
 func _physics_process(delta: float) -> void:
 	update_cam_pos(delta)
+	_handle_pan_zoom(delta)
 
-##Sets the value of the ground tilemap(s)
+##Resets the zoom to its default values.
+func reset_zoom():
+	zoom = init_zoom
+	_target_zoom = init_zoom
+
+##Sets the value of the ground tilemap(s).[br]
+##This is used to determine the level's bounds.
+##The tilemap must have the GroundTileMap script assigned.
 func set_ground() -> void:
 	#sets the value of ground to be the FIRST ground tilemap in the scene; if there is more than one, this will not work.
 	#TODO: Revisit this if there is ever going to be more than a single ground tilemap in a scene.[br]
@@ -53,13 +80,13 @@ func set_cam_limits():
 	global_position = player.global_position
 	#region cam limits variables
 	##the utilized rectangular enclosure of a scene
-	var used_rect = ground.get_used_rect()
+	var used_rect : Rect2i = ground.get_used_rect()
 	##the tile size in pixels for the defined ground tile set
-	var cell_size = ground.tile_set.tile_size
+	var cell_size : Vector2i = ground.tile_set.tile_size
 	##the total width of the ground layer
-	var map_width = used_rect.size.x * cell_size.x
+	var map_width : int = used_rect.size.x * cell_size.x
 	##the total height of the ground layer
-	var map_height = used_rect.size.y * cell_size.y
+	var map_height : int = used_rect.size.y * cell_size.y
 	#endregion
 	#region cam limits
 	limit_left = used_rect.position.x * cell_size.x
@@ -123,5 +150,50 @@ func update_cam_pos(delta):
 	#endregion clamp variables
 	
 	# smooth movement control; uses delta to move the camera at the player's speed
-	position.x = move_toward(position.x, target_pos.x, abs(player.velocity.x) * delta)
-	position.y = move_toward(position.y, target_pos.y, abs(player.velocity.y) * delta)
+	global_position.x = move_toward(position.x, target_pos.x, abs(player.velocity.x) * delta)
+	global_position.y = move_toward(position.y, target_pos.y, abs(player.velocity.y) * delta)
+
+##Addresses inputs from the player that allow the camera to move
+func _handle_pan_zoom(delta):
+	#region Zoom
+	var is_zooming = Input.is_action_pressed("camZoom")
+	if is_zooming:
+		_target_offset = Vector2.ZERO
+		var viewport_size = get_viewport_rect().size
+		##The current zoom level's width
+		var current_view_width = viewport_size.x / init_zoom.x
+		##The current zoom level's height
+		var current_view_height = viewport_size.y / init_zoom.y
+		##The destination zoom level's width
+		var target_view_width = current_view_width + (max_pan_distance.x / 2.0)
+		##The destination zoom level's hieght
+		var target_view_height = current_view_height + (max_pan_distance.y / 2.0)
+		
+		#Set the target view based on the difference between the current view and the goal view
+		_target_zoom = Vector2((viewport_size.x / target_view_width), (viewport_size.y / target_view_height))
+	#endregion Zoom
+	else:
+		_target_zoom = init_zoom
+	#region Pan
+		#get input from the player
+		var pan_input = Vector2((Input.get_action_strength("camRight")) - Input.get_action_strength("camLeft"), (Input.get_action_strength("camDown") - Input.get_action_strength("camUp")))
+		#set desired offset, cam position, and get the value of half the screen to determine its centerpoint
+		var desired_offset : Vector2 = pan_input * max_pan_distance
+		var base_cam_pos = global_position
+		var safe_zoom = Vector2(max(0.01, zoom.x), max(0.01, zoom.y))
+		var screen_half = (get_viewport_rect().size / safe_zoom) / 2.0
+		
+		#set permissible range
+		var allowed_left = max(0.0, (base_cam_pos.x - screen_half.x) - limit_left)
+		var allowed_right = max(0.0, limit_right - (base_cam_pos.x + screen_half.x))
+		var allowed_top = max(0.0, (base_cam_pos.y - screen_half.y) - limit_top)
+		var allowed_bottom = max(0.0, limit_bottom - (base_cam_pos.y + screen_half.y))
+		#establish target offset
+		_target_offset.x = clamp(desired_offset.x, -allowed_left, allowed_right)
+		_target_offset.y = clamp(desired_offset.y, -allowed_top, allowed_bottom)
+	#endregion Pan
+	#region Apply Zoom/Pan
+	offset = offset.lerp(_target_offset, smoothness * delta)
+	zoom = zoom.lerp(_target_zoom, smoothness * delta)
+	#endregion Apply Zoom/Pan
+#endregion
